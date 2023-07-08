bc49d7aad6b067873bcbd5ea18c5630a46658f3a
Seba <134440670+sebastian-lopez-p@users.noreply.github.com>
2023-07-07 22:52:55 -0400

    Update Test1.py
    Añadi las librerías
    __future, Pefefef

--- Test1.py
+++ Test1.py
@@ -1,3 +1,10 @@
+#librerías
+from __future__ import print_function, division
+from PyAstronomy import pyasl
+import numpy as np
+import matplotlib.pylab as plt
+from matplotlib.widgets import Slider, RadioButtons
 print("Hola Mundo")
 print("HeHe")
+
 #comentario bonito de prueba
